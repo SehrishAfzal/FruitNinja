e52ad925c411387960722092d1226de41bcf71ee
SehrishAfzal <80562961+SehrishAfzal@users.noreply.github.com>
2021-04-15 04:29:34 -0700

    assignment 2[lab]
    in this assignment we have to add the  animations in this game so in this  i create the  main frame and add players (in our game fruits and axe are included in players) and also perform some code implementation  (at notepad).

---
+++ animation file.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+       public float speed=6;
+      public float turnspeed=2;
+    
+ // Update is called once per frame
+    void Update()
+    {
+        float strafe= Input. GetAxis ("Horizontal");
+       float forward=Input.GetAxis("vertical");
+
+      transform.Translate=(new vector3(strafe, 0 ,forward)* speed *Time.deltaTime);
+
+     float turn = Input.GetAxis("Mouse X");
+
+    transform.rotation *= Quaternion.Slerp(Quaternion .indentity,Quaternion.LookRotation(turn < 0; vector3.left : vector3.right), Mathf.Abs( turn ) *  turnSpeed * Time.deltaTime);
+    }
+}
