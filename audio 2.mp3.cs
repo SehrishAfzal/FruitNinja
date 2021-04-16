45c97bb233e178cecf2bc465f9baabb9c12196ab
SehrishAfzal <80562961+SehrishAfzal@users.noreply.github.com>
2021-04-16 02:00:18 -0700

    ASSSIGNMENT 3 LAB
    THIS TASK IS DONE BY MY PATNER

---
+++ audio 2.mp3.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetBehaviour : MonoBehaviour
+{
+    private AudioSource source;
+    
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+    
+    void onCollisionEnter2D(Collision2D col) 
+    {
+        // target is hit
+        if(col.gameObject.tag == "Bullet")
+        {
+            Debug.Log("Target was Hit!");
+            
+            // calling AddScore method
+            ScoreBehaviour.AddScore();
+            
+            // play sound
+            source.Play();
+            
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+}
